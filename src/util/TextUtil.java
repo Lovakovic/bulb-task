ffcfa843bf4a58fc81016c2688599f932924f751
Lovakovic <lovkovdario@gmail.com>
2023-05-17 22:34:10 +0200

    Add Javadoc to methods

--- src/util/TextUtil.java
+++ src/util/TextUtil.java
@@ -7,7 +7,16 @@ import model.Word;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * A utility class providing methods for text manipulation. Each public method solves one task.
+ */
 public class TextUtil {
+    /**
+     * Randomizes the inner characters in the words of the given text, preserving the first and last characters.
+     *
+     * @param text The original text.
+     * @return The transformed text with the inner characters of the words randomized.
+     */
     public static String randomizeInnerChars(String text) {
         List<Sentence> sentences = parseTextToSentences(text);
         List<Sentence> modifiedSentences = new ArrayList<>();
@@ -39,6 +48,13 @@ public class TextUtil {
         return buildStringFromSentences(modifiedSentences).trim();
     }
 
+
+    /**
+     * Reverses the characters in the words of the given text.
+     *
+     * @param text The original text.
+     * @return The transformed text with the characters in the words reversed.
+     */
     public static String reverseCharsInWord(String text) {
         List<Sentence> sentences = parseTextToSentences(text);
         List<Sentence> modifiedSentences = new ArrayList<>();
@@ -61,6 +77,12 @@ public class TextUtil {
         return buildStringFromSentences(modifiedSentences);
     }
 
+    /**
+     * Reverses the order of words in the sentences of the given text.
+     *
+     * @param text The original text.
+     * @return The transformed text with the order of words in the sentences reversed.
+     */
     public static String reverseWordsInSentence(String text) {
         List<Sentence> sentences = parseTextToSentences(text);
         List<Sentence> modifiedSentences = new ArrayList<>();
@@ -83,6 +105,12 @@ public class TextUtil {
         return buildStringFromSentences(modifiedSentences);
     }
 
+    /**
+     * Counts the number of each vowel character in the given text.
+     *
+     * @param text The original text.
+     * @return A map with the vowel characters as keys and their counts as values.
+     */
     public static Map<Character, Long> countVowelsInText(String text) {
         List<Sentence> sentences = parseTextToSentences(text);
 
@@ -104,6 +132,12 @@ public class TextUtil {
         return textVowelCount;
     }
 
+    /**
+     * Counts the number of vowels in each sentence of the given text.
+     *
+     * @param text The original text.
+     * @return A string summarizing the count of vowels in each sentence.
+     */
     public static String countVowelsInEachSentence(String text) {
         List<Sentence> sentences = parseTextToSentences(text);
         StringBuilder stringBuilder = new StringBuilder();
@@ -116,18 +150,36 @@ public class TextUtil {
         return stringBuilder.toString();
     }
 
+    /**
+     * Reverses the order of sentences in the given text.
+     *
+     * @param text The original text.
+     * @return The transformed text with the order of sentences reversed.
+     */
     public static String reverseSentencesInText(String text) {
         List<Sentence> sentences = new ArrayList<>(parseTextToSentences(text));
         Collections.reverse(sentences);
         return buildStringFromSentences(sentences);
     }
 
+    /**
+     * Splits the given text into sentences.
+     *
+     * @param text The original text.
+     * @return A list of sentences.
+     */
     private static List<Sentence> parseTextToSentences(String text) {
         return Arrays.stream(text.replace("\n", " ").split("(?<=[.!?])\\s+"))
                 .map(Sentence::new)
                 .toList();
     }
 
+    /**
+     * Builds a string from the given list of sentences.
+     *
+     * @param sentences The list of sentences.
+     * @return The text built from the list of sentences.
+     */
     private static String buildStringFromSentences(List<Sentence> sentences) {
         return sentences.stream().map(Sentence::toString).collect(Collectors.joining());
     }
